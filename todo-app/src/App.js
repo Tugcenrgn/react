5fa409389df0795401a5eaa29e0879eaf407a05c
safaksenoleem <114605881+safaksenoleem@users.noreply.github.com>
2022-12-25 21:29:08 +0300

    Todo-app
    React-Javascript-Css-Uuid

---
+++ todo-app/src/App.js
@@ -0,0 +1,47 @@
+import { useState } from 'react';
+import './App.css';
+import TodoField from './components/TodoField';
+import Todos from './components/Todos';
+
+function App() {
+
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      name: "Alışveriş",
+      status: false,
+    },
+    {
+      id: 2,
+      name: "Gym",
+      status: true,
+    },
+  ]);
+
+  const addTodo = (todo) => {
+    if (todo.name) {
+      setTodos([...todos, todo]);
+    }
+  };
+
+  const deleteTodo = (id) => {
+    let newTodos = todos.filter((todo) => todo.id !== id);
+    setTodos(newTodos);
+  }
+
+  const doneTodo = (id) => {
+    let currentTodo = todos.find((todo) =>
+      todo.id === id);
+    currentTodo.status = true;
+    setTodos([...todos]);
+  }
+  return (
+    <div className="App">
+      <h1>Todos</h1>
+      <TodoField addTodo={addTodo} />
+      <Todos doneTodo={doneTodo} deleteTodo={deleteTodo} todos={todos} />
+    </div>
+  );
+};
+
+export default App;
