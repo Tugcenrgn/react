b775d5562f6af808b98372cf5f96f80708dadb21
safaksenoleem <114605881+safaksenoleem@users.noreply.github.com>
2023-01-05 00:24:36 +0300

    update calculator-app
    react-js-mathjs-css

--- calculator-app/src/App.js
+++ calculator-app/src/App.js
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import * as math from "mathjs";
 import './App.css';
 import Button from './components/Button';
 import Input from './components/Input';
@@ -8,43 +9,50 @@ function App() {
   const [text, setText] = useState("");
   const [result, setResult] = useState("");
 
-  const addToText = (val) => {
+  const addToValue = (val) => {
     setText((text)=>[...text,val+" "])
   }
   
-  const resetInput = () => {
+  const resInput = () => {
     setText("");
     setResult("");
   }
+
+  const calcResult = () => {
+    const input = text.join("")
+    setResult(math.evaluate(input))
+    setText("")
+  }
+
   return (
     <div className="container">
       <div className="calc-wrap">
         <Input text={text} result={result} />
         <div className="but-row">
-          <Button symbol="7" handleClick={addToText}/>
-          <Button symbol="8" handleClick={addToText}/>
-          <Button symbol="9" handleClick={addToText}/>
-          <Button symbol="/" color="#00AD85" handleClick={addToText}/>
+          <Button symbol="7" handleClick={addToValue}/>
+          <Button symbol="8" handleClick={addToValue}/>
+          <Button symbol="9" handleClick={addToValue}/>
+          <Button symbol="/" color="#00AD85" handleClick={addToValue}/>
         </div>
          <div className="but-row">
-          <Button symbol="4" handleClick={addToText}/>
-          <Button symbol="5" handleClick={addToText}/>
-          <Button symbol="6" handleClick={addToText}/>
-          <Button symbol="*" color="#00AD85" handleClick={addToText}/>
+          <Button symbol="4" handleClick={addToValue}/>
+          <Button symbol="5" handleClick={addToValue}/>
+          <Button symbol="6" handleClick={addToValue}/>
+          <Button symbol="*" color="#00AD85" handleClick={addToValue}/>
         </div>
          <div className="but-row">
-          <Button symbol="1" handleClick={addToText}/>
-          <Button symbol="2" handleClick={addToText}/>
-          <Button symbol="3" handleClick={addToText}/>
-          <Button symbol="+" color="#00AD85" handleClick={addToText}/>
+          <Button symbol="1" handleClick={addToValue}/>
+          <Button symbol="2" handleClick={addToValue}/>
+          <Button symbol="3" handleClick={addToValue}/>
+          <Button symbol="+" color="#00AD85" handleClick={addToValue}/>
         </div>
          <div className="but-row">
-          <Button symbol="0" handleClick={addToText}/>
-          <Button symbol="." handleClick={addToText}/>
-          <Button symbol="=" />
-          <Button symbol="-" color="#00AD85" handleClick={addToText}/>
+          <Button symbol="0" handleClick={addToValue}/>
+          <Button symbol="." handleClick={addToValue}/>
+          <Button symbol="=" handleClick={calcResult}/>
+          <Button symbol="-" color="#00AD85" handleClick={addToValue}/>
         </div>
-        <Button symbol="Clear" color="red"/>
+        <Button symbol="Clear" color="red" handleClick={resInput}/>
       </div>
     </div>
     
