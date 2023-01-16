46ad8bf4270314e0654e4d392857b270f97f8fa9
safaksenoleem <114605881+safaksenoleem@users.noreply.github.com>
2023-01-16 16:01:59 +0300

    update chatgpt
    add the axios

--- chatgpt-app/src/App.js
+++ chatgpt-app/src/App.js
@@ -1,7 +1,34 @@
 import "./App.css";
+import axios from "axios";
 
 function App() {
-  return <div className="App">Chatgpt-App</div>;
-}
+  const [prompt, setPrompt] = useState("");
+  const [response, setResponse] = useState("");
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    axios
+      .post("/chat", { prompt })
+      .then((res) => {
+        setResponse(res.data);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={prompt}
+          onChange={(e) => setPrompt(e.target.value)}
+        />
+        <button type="submit">Submit</button>
+      </form>
+      <p>{response}</p>
+    </div>
+  );
+}
 export default App;
