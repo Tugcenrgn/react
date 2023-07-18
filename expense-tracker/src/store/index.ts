496adbca17e4716b0571f43640be74bc21022b57
Tuğçe Nur Ergen <tugce.ergen@atmosware.turkcell.com.tr>
2023-07-18 17:14:56 +0300

    updates

---
+++ expense-tracker/src/store/index.ts
@@ -0,0 +1,17 @@
+import { combineReducers } from "redux";
+import userReducer from "./reducers/userReducer";
+import { UserState } from "../types/user";
+
+interface AppState{
+    user: UserState;
+    // categories: any;
+    // records: any;
+}
+
+const rootReducer = combineReducers<AppState>({
+    user: userReducer,
+    // categories: () => {},
+    // records: () => {},
+});
+
+export default rootReducer;
